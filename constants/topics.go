90c2a963c403e3d30d4a02af9f3fe310e0c32322
lbtsm <able@gamil.com>
2025-05-19 11:30:08 +0800

    add topic file

---
+++ constants/topics.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	EventOfMapRelay      = "0xf01fbdd2fdbc5c2f201d087d588789d600e38fe56427e813d9dced2cdb25bcac"
+	EventOfMessageOut    = "0x469059a9fd182ad3741bdd67b925e15056d35262609ea83393db7e8fb5a05ab1"
+	EventOfSolCrossOut   = "crossOut"
+	EventOfBTcMessageOut = "0x2aaebc938a5ab70e98644b0c6a8472fe02b7edece7e6e6dc71959dc34e109dfc"
+)
