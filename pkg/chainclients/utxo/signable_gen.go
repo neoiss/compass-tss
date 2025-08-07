a7a785b1b330bedad0496fc70adedb916ea86f09
neoiss <neoiss@163.com>
2025-08-07 15:37:59 +0800

    refactor(utxo): refactor BTC's GetPubKey method

--- pkg/chainclients/utxo/signable_gen.go
+++ pkg/chainclients/utxo/signable_gen.go
@@ -4,19 +4,20 @@ package utxo
 
 import (
 	"encoding/base64"
-  "fmt"
+	"encoding/hex"
+	"fmt"
 	"math/big"
 
+	btcec "github.com/btcsuite/btcd/btcec"
 	"github.com/cosmos/cosmos-sdk/crypto/codec"
+	dogeec "github.com/eager7/dogd/btcec"
+	bchec "github.com/gcash/bchd/bchec"
+	ltcec "github.com/ltcsuite/ltcd/btcec"
 	"github.com/rs/zerolog"
-  bchec "github.com/gcash/bchd/bchec"
-  btcec "github.com/btcsuite/btcd/btcec"
-  dogeec "github.com/eager7/dogd/btcec"
-  ltcec "github.com/ltcsuite/ltcd/btcec"
 
-	"github.com/mapprotocol/compass-tss/tss"
 	"github.com/mapprotocol/compass-tss/common"
 	"github.com/mapprotocol/compass-tss/common/cosmos"
+	"github.com/mapprotocol/compass-tss/tss"
 )
 
 type tssSignableBCH struct {
@@ -109,17 +110,23 @@ func (ts *tssSignableBTC) Sign(payload []byte) (*btcec.Signature, error) {
 }
 
 func (ts *tssSignableBTC) GetPubKey() *btcec.PublicKey {
-	cpk, err := cosmos.GetPubKeyFromBech32(cosmos.Bech32PubKeyTypeAccPub, ts.poolPubKey.String())
+	//cpk, err := cosmos.GetPubKeyFromBech32(cosmos.Bech32PubKeyTypeAccPub, ts.poolPubKey.String())
+	//if err != nil {
+	//	ts.log.Err(err).Str("pubkey", ts.poolPubKey.String()).Msg("fail to get pubic key from the bech32 pool public key string")
+	//	return nil
+	//}
+	//secpPubKey, err := codec.ToCmtPubKeyInterface(cpk)
+	//if err != nil {
+	//	ts.log.Err(err).Msgf("%s is not a secp256 k1 public key", ts.poolPubKey)
+	//	return nil
+	//}
+	pubKey, err := hex.DecodeString(ts.poolPubKey.String())
 	if err != nil {
-		ts.log.Err(err).Str("pubkey", ts.poolPubKey.String()).Msg("fail to get pubic key from the bech32 pool public key string")
+		ts.log.Err(err).Msg("fail to decode public key")
 		return nil
 	}
-	secpPubKey, err := codec.ToCmtPubKeyInterface(cpk)
-	if err != nil {
-		ts.log.Err(err).Msgf("%s is not a secp256 k1 public key", ts.poolPubKey)
-		return nil
-	}
-	newPubkey, err := btcec.ParsePubKey(secpPubKey.Bytes(), btcec.S256())
+	//newPubkey, err := btcec.ParsePubKey(secpPubKey.Bytes(), btcec.S256())
+	newPubkey, err := btcec.ParsePubKey(pubKey, btcec.S256())
 	if err != nil {
 		ts.log.Err(err).Msg("fail to parse public key")
 		return nil
